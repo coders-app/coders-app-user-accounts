8c576e2ce2a57d4e1aeafcfad77b8430cac7dd1a
Lorena Martinez Moledo <lmartmol@gmail.com>
2023-02-06 16:30:05 +0100

    Add LoginForm html skeleton & styles with type funcionality

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import LoginForm from "../../components/LoginForm/LoginForm";
 import LoginPageStyled from "./LoginPageStyled";
 
 const LoginPage = () => {
   return (
     <LoginPageStyled>
       <h1>Login</h1>
+      <LoginForm />
     </LoginPageStyled>
   );
 };
