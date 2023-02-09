42a6a58eee0445f1f1b0446007f06ff527614cfd
Lorena Martinez Moledo <lmartmol@gmail.com>
2023-02-09 17:57:17 +0100

    Add LoginPage test show a LoginForm component with customRender

--- src/pages/LoginPage/LoginPage.test.tsx
+++ src/pages/LoginPage/LoginPage.test.tsx
@@ -1,5 +1,5 @@
-import { render, screen } from "@testing-library/react";
-import WrapperWithProviders from "../../testUtils/wrappers/WrapperWithProviders";
+import { screen } from "@testing-library/react";
+import customRender from "../../testUtils/customRender";
 import LoginPage from "./LoginPage";
 
 describe("Given the page LoginPage", () => {
@@ -10,10 +10,30 @@ describe("Given the page LoginPage", () => {
         name: /login/i,
       };
 
-      render(<LoginPage />, { wrapper: WrapperWithProviders });
+      customRender(<LoginPage />);
       const renderedHeading = screen.getByRole("heading", expectedHeading);
 
       expect(renderedHeading).toBeInTheDocument();
+    });
+
+    test("Then it should show a form with 'Email' and 'Password' as a text box and a button with text 'Send'", () => {
+      const emailLabel = /email/i;
+      const passwordLabel = /password/i;
+      const buttonText = /send/i;
+
+      customRender(<LoginPage />);
+
+      const resultEmailLabel: HTMLLabelElement =
+        screen.getByLabelText(emailLabel);
+      const resultPasswordLabel: HTMLLabelElement =
+        screen.getByLabelText(passwordLabel);
+      const resultButton: HTMLButtonElement = screen.getByRole("button", {
+        name: buttonText,
+      });
+
+      expect(resultEmailLabel).toBeInTheDocument();
+      expect(resultPasswordLabel).toBeInTheDocument();
+      expect(resultButton).toBeInTheDocument();
     });
   });
 });
