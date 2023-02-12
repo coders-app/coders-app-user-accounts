2f889ee1bb8aaff66351b102b2194e8661ce9aae
Lorena Martinez Moledo <lmartmol@gmail.com>
2023-02-12 14:49:18 +0100

    Update isFormValid naming & specify handleOnSubmit event type

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,4 +1,4 @@
-import { SyntheticEvent, useState } from "react";
+import { useState } from "react";
 import { UserCredentials } from "../../store/models/User";
 import LoginFormStyled from "./LoginFormStyled";
 
@@ -17,13 +17,12 @@ const LoginForm = (): JSX.Element => {
     });
   };
 
-  const handleOnSubmit = (event: SyntheticEvent) => {
+  const handleOnSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     setCredentials(initialCredentials);
   };
 
-  const formIsValidate = (): boolean =>
-    credentials.email !== "" && credentials.password !== "";
+  const isFormValid = credentials.email !== "" && credentials.password !== "";
 
   return (
     <LoginFormStyled noValidate autoComplete="off" onSubmit={handleOnSubmit}>
@@ -53,7 +52,7 @@ const LoginForm = (): JSX.Element => {
         ></input>
         <span className="form-group__message">Tot malament</span>
       </div>
-      <button className="button" type="submit" disabled={!formIsValidate()}>
+      <button className="button" type="submit" disabled={!isFormValid}>
         Send
       </button>
     </LoginFormStyled>
