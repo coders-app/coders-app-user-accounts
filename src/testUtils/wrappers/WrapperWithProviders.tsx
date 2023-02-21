f1935ae6c2207bf10047e06ef4263b76775f5f65
Catalina Tarelli <catatarelli@hotmail.com>
2023-02-21 16:41:43 +0100

    Add error handlers to test catch case of useUser

--- src/testUtils/wrappers/WrapperWithProviders.tsx
+++ src/testUtils/wrappers/WrapperWithProviders.tsx
@@ -1,13 +1,16 @@
 import { PropsWithChildren } from "react";
 import { ThemeProvider } from "styled-components";
+import UiContextProvider from "../../store/contexts/UiContext/UiContextProvider";
 import GlobalStyle from "../../styles/GlobalStyle";
 import mainTheme from "../../styles/mainTheme";
 
 const WrapperWithProviders = ({ children }: PropsWithChildren): JSX.Element => (
-  <ThemeProvider theme={mainTheme}>
-    <GlobalStyle />
-    {children}
-  </ThemeProvider>
+  <UiContextProvider>
+    <ThemeProvider theme={mainTheme}>
+      <GlobalStyle />
+      {children}
+    </ThemeProvider>
+  </UiContextProvider>
 );
 
 export default WrapperWithProviders;
