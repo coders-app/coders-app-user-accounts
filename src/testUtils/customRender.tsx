8bdd1a5aeb1e7ed9a9563070d6f2ec59fc772f03
Lorena Martinez Moledo <lmartmol@gmail.com>
2023-03-01 13:32:42 +0100

    Add UiContextProvider and UserContextProvider to customRender

--- src/testUtils/customRender.tsx
+++ src/testUtils/customRender.tsx
@@ -1,13 +1,17 @@
 import { render, RenderOptions } from "@testing-library/react";
+import { WrapperWithProvidersProps } from "./types";
 import WrapperWithProviders from "./wrappers/WrapperWithProviders";
 
 const customRender = (
   ui: React.ReactElement,
+  wrapperOptions?: WrapperWithProvidersProps,
   renderOptions?: RenderOptions
 ) => {
   return {
     ...render(ui, {
-      wrapper: (props) => <WrapperWithProviders {...props} />,
+      wrapper: (props) => (
+        <WrapperWithProviders {...props} {...wrapperOptions} />
+      ),
       ...renderOptions,
     }),
   };
