d59e0703a65400ae75517e8d0b8faeba761d9fe6
Berto <bertoforteza87@gmail.com>
2023-03-07 13:27:56 +0100

    Fix UseUserStructure to make funcitions return promises instead of void

--- src/hooks/types.ts
+++ src/hooks/types.ts
@@ -1,6 +1,6 @@
 import { UserCredentials } from "../types";
 
 export interface UseUserStructure {
-  getLoginCookie: (userCredentials: UserCredentials) => void;
-  verifyUser: () => void;
+  getLoginCookie: (userCredentials: UserCredentials) => Promise<void>;
+  verifyUser: () => Promise<void>;
 }
