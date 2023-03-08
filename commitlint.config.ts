87fd5a15cd264b2beed3713ff01ef1c07ff3dd34
Lorena Martinez Moledo <lmartmol@gmail.com>
2023-03-08 17:39:18 +0100

    Install commitlint & add npx commitlint in commit-msg hook

---
+++ commitlint.config.ts
@@ -0,0 +1,16 @@
+import type { UserConfig } from "@commitlint/types";
+
+const Configuration: UserConfig = {
+  extends: ["@commitlint/config-conventional"],
+  formatter: "@commitlint/format",
+  rules: {
+    "header-min-length": [2, "always", 10],
+    "header-max-length": [2, "always", 72],
+    "header-case": [2, "always", ["sentence-case"]],
+    "type-empty": [2, "always"],
+    "scope-empty": [2, "always"],
+    "subject-empty": [2, "always"],
+  },
+};
+
+module.exports = Configuration;
